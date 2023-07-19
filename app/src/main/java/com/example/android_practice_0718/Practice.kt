93c7b96404c7313eab0271ad67a219b7a351532b
doumin <aishi1026@gmail.com>
2023-07-19 09:39:32 +0900

    scanner_login

--- app/src/main/java/com/example/android_practice_0718/Practice.kt
+++ app/src/main/java/com/example/android_practice_0718/Practice.kt
@@ -2,8 +2,35 @@ package com.example.android_practice_0718
 
 import java.util.*
 
-fun main(){
 
+// data class - 임시 DTO(=VO), 데이터 전달 하기 위한 객체
+// 클래스에서 만들어서 사용할 때, 주 생성자를 이용하는데, 매개변수 부분에 변수 앞에 val 라는 키워드를 사용해서
+// 전역으로 사용하는 부분.
+data class User(val id: String, val pw: String) {
+
+}
+
+// 로그인 클래스 구현
+class LoginTest {
+    companion object {
+        // 클래스 함수 -> 클래스명.함수
+        fun login(user: User) {
+
+            if (user.id.equals("admin") && user.pw.equals("1234")) {
+                println("로그인 성공")
+            } else {
+                println("로그인 실패")
+            }
+        }
+
+    }
+}
+
+fun main() {
+
+    // 코틀린 : 모든 타입 객체
+    // 변수 선언 =>  var(val) 변수명 : 타입 = 해당값 할당 형식
+    // ide 1차 문법 체크
     val sc = Scanner(System.`in`)
 
     println("=회원 가입===============")
@@ -11,36 +38,14 @@ fun main(){
     val id = sc.nextLine()
     println("PW 입력")
     val pw = sc.nextLine()
-    println("email 입력")
-    val email = sc.nextLine()
-    println("phone 입력")
-    val phone = sc.nextLine()
+    val user: User = User(id, pw)
 
     println("=======================")
     println("회원 정보:")
-    println("id : $id")
-    println("pw : $pw")
-    println("email : $email")
-    println("phone : $phone")
-
-    println("=======================")
-    println("로그인")
-    println("ID : ")
-    val scanId = sc.nextLine()
-    println("PW : ")
-    val scanPw = sc.nextLine()
+    println(user)
 
     println("=======================")
 
-    if(id == scanId && pw == scanPw){
-        println("로그인 성공")
-    } else {
-        println("로그인 실패")
-    }
-}
-
-class Practice {
-
-
+    LoginTest.login(user)
 
 }
